76f8d1a226805da5fe36279b8cdbdce1be0f7c7c
Sebastiá <97sebastiangb@gmail.com>
2023-09-01 18:21:40 +0200

    Encode body in request url

--- src/client.ts
+++ src/client.ts
@@ -20,7 +20,7 @@ export default class Client {
             body['clientId'] = clientId
         }
 
-        const response = await fetch(`${this.data?.baseUrl}${endpoint}`, {
+        const response = await fetch(`${this.data?.baseUrl}${endpoint}?clientId=${clientId}&accessToken=${parameters.accessToken}&${objectToQueryString(body)}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -35,3 +35,20 @@ export default class Client {
         return responseJSON
     }
 }
+
+function objectToQueryString(obj: any) {
+    const queryString = [];
+  
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+        // Ensure the value is properly encoded
+        const encodedValue = encodeURIComponent(value);
+        // Add the key-value pair to the query string array
+        queryString.push(`${key}=${encodedValue}`);
+      }
+    }
+  
+    // Join the key-value pairs with "&" to form the complete query string
+    return queryString.join('&');
+  }
